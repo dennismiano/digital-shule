b60249f99bb31542b01c8e1c104536dfd6dc1f4c
KoiKoishogun <sydneysayeed@gmail.com>
2018-02-05 13:08:04 +0300

    Creatd default examModule view where everything happens

---
+++ resources/views/examModule/view.blade.php
@@ -0,0 +1,13 @@
+{{-- This is the default examModule view everything will happen on this view. --}}
+
+<div>
+</div>
+
+
+
+
+
+
+
+
+{{-- END .....This is the default examModule view everything will happen on this view.   END....--}}
